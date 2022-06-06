6396e9d1563d29365d549797ee4ebc16380e22f4
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-06-06 15:47:17 -0400

    test

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import torch
 from torch_geometric.data import Data
 
+print(torch.__version__)
+print(torch.version.cuda)
+
 edge_index = torch.tensor([[0, 1, 1, 2],
                            [1, 0, 2, 1]], dtype=torch.long)
 x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
