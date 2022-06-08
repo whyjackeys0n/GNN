8e9111cc2f2a61a2302b6737515e16d90b5800cd
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-06-07 23:26:37 -0400

    test

--- GCN/load_graph.py
+++ GCN/load_graph.py
@@ -1,199 +1,111 @@
-# import packages
-
-# general tools
-import numpy as np
-
-# RDkit
-from rdkit import Chem
-from rdkit.Chem.rdmolops import GetAdjacencyMatrix
-
-# Pytorch and Pytorch Geometric
+import rdkit
+from torch_geometric.datasets import MoleculeNet
 import torch
-from torch_geometric.data import Data
-from torch.utils.data import DataLoader
-
-
-def one_hot_encoding(x, permitted_list):
-    """
-    Maps input elements x which are not in the permitted list to the last element
-    of the permitted list.
-    """
-
-    if x not in permitted_list:
-        x = permitted_list[-1]
-
-    binary_encoding = [int(boolean_value) for boolean_value in list(map(lambda s: x == s, permitted_list))]
-
-    return binary_encoding
-
-
-def get_atom_features(atom,
-                      use_chirality=True,
-                      hydrogens_implicit=True):
-    """
-    Takes an RDKit atom object as input and gives a 1d-numpy array of atom features as output.
-    """
-
-    # define list of permitted atoms
-
-    permitted_list_of_atoms = ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As', 'Al', 'I',
-                               'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'Li', 'Ge',
-                               'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr', 'Pt', 'Hg', 'Pb', 'Unknown']
-
-    if hydrogens_implicit == False:
-        permitted_list_of_atoms = ['H'] + permitted_list_of_atoms
-
-    # compute atom features
-
-    atom_type_enc = one_hot_encoding(str(atom.GetSymbol()), permitted_list_of_atoms)
-
-    n_heavy_neighbors_enc = one_hot_encoding(int(atom.GetDegree()), [0, 1, 2, 3, 4, "MoreThanFour"])
-
-    formal_charge_enc = one_hot_encoding(int(atom.GetFormalCharge()), [-3, -2, -1, 0, 1, 2, 3, "Extreme"])
-
-    hybridisation_type_enc = one_hot_encoding(str(atom.GetHybridization()),
-                                              ["S", "SP", "SP2", "SP3", "SP3D", "SP3D2", "OTHER"])
-
-    is_in_a_ring_enc = [int(atom.IsInRing())]
-
-    is_aromatic_enc = [int(atom.GetIsAromatic())]
-
-    atomic_mass_scaled = [float((atom.GetMass() - 10.812) / 116.092)]
-
-    vdw_radius_scaled = [float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum()) - 1.5) / 0.6)]
-
-    covalent_radius_scaled = [float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()) - 0.64) / 0.76)]
-
-    atom_feature_vector = atom_type_enc + n_heavy_neighbors_enc + formal_charge_enc + hybridisation_type_enc + is_in_a_ring_enc + is_aromatic_enc + atomic_mass_scaled + vdw_radius_scaled + covalent_radius_scaled
-
-    if use_chirality == True:
-        chirality_type_enc = one_hot_encoding(str(atom.GetChiralTag()),
-                                              ["CHI_UNSPECIFIED", "CHI_TETRAHEDRAL_CW", "CHI_TETRAHEDRAL_CCW",
-                                               "CHI_OTHER"])
-        atom_feature_vector += chirality_type_enc
-
-    if hydrogens_implicit == True:
-        n_hydrogens_enc = one_hot_encoding(int(atom.GetTotalNumHs()), [0, 1, 2, 3, 4, "MoreThanFour"])
-        atom_feature_vector += n_hydrogens_enc
-
-    return np.array(atom_feature_vector)
-
-
-def get_bond_features(bond,
-                      use_stereochemistry=True):
-    """
-    Takes an RDKit bond object as input and gives a 1d-numpy array of bond features as output.
-    """
-
-    permitted_list_of_bond_types = [Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE,
-                                    Chem.rdchem.BondType.TRIPLE, Chem.rdchem.BondType.AROMATIC]
-
-    bond_type_enc = one_hot_encoding(bond.GetBondType(), permitted_list_of_bond_types)
-
-    bond_is_conj_enc = [int(bond.GetIsConjugated())]
-
-    bond_is_in_ring_enc = [int(bond.IsInRing())]
-
-    bond_feature_vector = bond_type_enc + bond_is_conj_enc + bond_is_in_ring_enc
-
-    if use_stereochemistry == True:
-        stereo_type_enc = one_hot_encoding(str(bond.GetStereo()), ["STEREOZ", "STEREOE", "STEREOANY", "STEREONONE"])
-        bond_feature_vector += stereo_type_enc
-
-    return np.array(bond_feature_vector)
-
-
-def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y):
-    """
-    Inputs:
-
-    x_smiles = [smiles_1, smiles_2, ....] ... a list of SMILES strings
-    y = [y_1, y_2, ...] ... a list of numerial labels for the SMILES strings (such as associated pKi values)
-
-    Outputs:
-
-    data_list = [G_1, G_2, ...] ... a list of torch_geometric.data.Data objects which represent labeled molecular graphs that can readily be used for machine learning
-
-    """
-
-    data_list = []
-
-    for (smiles, y_val) in zip(x_smiles, y):
-
-        # convert SMILES to RDKit mol object
-        mol = Chem.MolFromSmiles(smiles)
-
-        # get feature dimensions
-        n_nodes = mol.GetNumAtoms()
-        n_edges = 2 * mol.GetNumBonds()
-        unrelated_smiles = "O=O"
-        unrelated_mol = Chem.MolFromSmiles(unrelated_smiles)
-        n_node_features = len(get_atom_features(unrelated_mol.GetAtomWithIdx(0)))
-        n_edge_features = len(get_bond_features(unrelated_mol.GetBondBetweenAtoms(0, 1)))
-
-        # construct node feature matrix X of shape (n_nodes, n_node_features)
-        X = np.zeros((n_nodes, n_node_features))
-
-        for atom in mol.GetAtoms():
-            X[atom.GetIdx(), :] = get_atom_features(atom)
-
-        X = torch.tensor(X, dtype=torch.float)
-
-        # construct edge index array E of shape (2, n_edges)
-        (rows, cols) = np.nonzero(GetAdjacencyMatrix(mol))
-        torch_rows = torch.from_numpy(rows.astype(np.int64)).to(torch.long)
-        torch_cols = torch.from_numpy(cols.astype(np.int64)).to(torch.long)
-        E = torch.stack([torch_rows, torch_cols], dim=0)
-
-        # construct edge feature array EF of shape (n_edges, n_edge_features)
-        EF = np.zeros((n_edges, n_edge_features))
-
-        for (k, (i, j)) in enumerate(zip(rows, cols)):
-            EF[k] = get_bond_features(mol.GetBondBetweenAtoms(int(i), int(j)))
-
-        EF = torch.tensor(EF, dtype=torch.float)
-
-        # construct label tensor
-        y_tensor = torch.tensor(np.array([y_val]), dtype=torch.float)
-
-        # construct Pytorch Geometric data object and append to data list
-        data_list.append(Data(x=X, edge_index=E, edge_attr=EF, y=y_tensor))
-
-    return data_list
-
-
-# canonical training loop for a Pytorch Geometric GNN model gnn_model
-
-# create list of molecular graph objects from list of SMILES x_smiles and list of labels y
-data_list = create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y)
-
-# create dataloader for training
-dataloader = DataLoader(dataset = data_list, batch_size = 2**7)
-
-# define loss function
-loss_function = nn.MSELoss()
-
-# define optimiser
-optimiser = torch.optim.Adam(gnn_model.parameters(), lr = 1e-3)
-
-# loop over 10 training epochs
-for epoch in range(10):
-
-    # set model to training mode
-    gnn_model.train()
-
-    # loop over minibatches for training
-    for (k, batch) in enumerate(dataloader):
-
-        # compute current value of loss function via forward pass
-        output = gnn_model(batch)
-        loss_function_value = loss_function(output[:,0], torch.tensor(batch.y, dtype = torch.float32))
-
-        # set past gradient to zero
-        optimiser.zero_grad()
-
-        # compute current gradient via backward pass
-        loss_function_value.backward()
-
-        # update model weights using gradient and optimisation method
-        optimiser.step()
+from torch.nn import Linear
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, TopKPooling, global_mean_pool
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+
+# Load the ESOL dataset
+data = MoleculeNet(root='.', name='ESOL')
+
+# Investigating the dataset
+print("Dataset type:", type(data))
+print("Dataset features:", data.num_features)
+print("Dataset target:", data.num_classes)
+print("Dataset length:", data.len)
+print("Dataset sample:", data[0])
+print("Sample nodes:", data[0].num_nodes)
+print("Sample edges:", data[0].num_edges)
+
+embedding_size = 64
+
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        # Init parent
+        super(GCN, self).__init__()
+        torch.manual_seed(42)
+
+        # GCN layers
+        self.initial_conv = GCNConv(data.num_features, embedding_size)
+        self.conv1 = GCNConv(embedding_size, embedding_size)
+        self.conv2 = GCNConv(embedding_size, embedding_size)
+        self.conv3 = GCNConv(embedding_size, embedding_size)
+
+        # Output layer
+        self.out = Linear(embedding_size * 2, 1)
+
+    def forward(self, x, edge_index, batch_index):
+        # First Conv layer
+        hidden = self.initial_conv(x, edge_index)
+        hidden = F.tanh(hidden)
+
+        # Other Conv layers
+        hidden = self.conv1(hidden, edge_index)
+        hidden = F.tanh(hidden)
+        hidden = self.conv2(hidden, edge_index)
+        hidden = F.tanh(hidden)
+        hidden = self.conv3(hidden, edge_index)
+        hidden = F.tanh(hidden)
+
+        # Global Pooling (stack different aggregations)
+        hidden = torch.cat([gmp(hidden, batch_index),
+                            gap(hidden, batch_index)], dim=1)
+
+        # Apply a final (linear) classifier.
+        out = self.out(hidden)
+
+        return out, hidden
+
+
+model = GCN()
+print(model)
+print("Number of parameters: ", sum(p.numel() for p in model.parameters()))
+
+from torch_geometric.data import DataLoader
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# Root mean squared error
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0007)
+
+# Use GPU for training
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+# Wrap data in a data loader
+data_size = len(data)
+NUM_GRAPHS_PER_BATCH = 64
+loader = DataLoader(data[:int(data_size * 0.8)],
+                    batch_size=NUM_GRAPHS_PER_BATCH, shuffle=True)
+test_loader = DataLoader(data[int(data_size * 0.8):],
+                         batch_size=NUM_GRAPHS_PER_BATCH, shuffle=True)
+
+
+def train(data):
+    # Enumerate over the data
+    for batch in loader:
+        # Use GPU
+        batch.to(device)
+        # Reset gradients
+        optimizer.zero_grad()
+        # Passing the node features and the connection info
+        pred, embedding = model(batch.x.float(), batch.edge_index, batch.batch)
+        # Calculating the loss and gradients
+        loss = loss_fn(pred, batch.y)
+        loss.backward()
+        # Update using the gradients
+        optimizer.step()
+    return loss, embedding
+
+
+print("Starting training...")
+losses = []
+for epoch in range(2000):
+    loss, h = train(data)
+    losses.append(loss)
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch} | Train Loss {loss}")
