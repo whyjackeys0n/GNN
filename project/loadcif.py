6739c8aa19a52763c7da1e9583bd05e1d3d686f6
Jiacheng Wang <jiachengwang@umass.edu>
2022-06-17 16:25:50 -0400

    test

--- project/loadcif.py
+++ project/loadcif.py
@@ -4,11 +4,20 @@ from pymatgen.core.structure import Structure, Molecule
 from pymatgen.analysis.graphs import StructureGraph
 from pymatgen.analysis.local_env import JmolNN
 
-mol = next(pybel.readfile("cif", "./data/raw/Li7La3Zr2O12.cif"))
-pybelmol = pybel.Molecule(mol)
-pybelmol.write("sdf", "outputfile.sdf", overwrite=True)
-rdmol = Chem.SDMolSupplier('outputfile.sdf')
-#
+mol1 = next(pybel.readfile("cif", "./data/raw/Li7La3Zr2O12.cif"))
+mol2 = next(pybel.readfile("cif", "./data/raw/LiLa5Ti8O24.cif"))
+# pybelmol = pybel.Molecule(mol)
+# pybelmol.write("sdf", "outputfile.sdf", overwrite=True)
+pybelmol = pybel.Outputfile("sdf", "outputfile.sdf", overwrite=True)
+pybelmol.write(mol1)
+pybelmol.write(mol2)
+pybelmol.close()
+
+
+suppl = Chem.SDMolSupplier('outputfile.sdf')
+
+mol1 = next(suppl)
+
 # for atom in rdmol.GetAtoms():
 #     print(atom.GetAtomicNum())  # Atom number
 
@@ -16,3 +25,5 @@ rdmol = Chem.SDMolSupplier('outputfile.sdf')
 structure = Structure.from_file("data\\raw\\Li7La3Zr2O12.cif")
 sg = StructureGraph.with_local_env_strategy(structure, JmolNN())
 g = sg.graph
+
+
