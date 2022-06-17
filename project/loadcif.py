e50e0e8184c09b5b4676b25c110e3c086a0a5240
Jiacheng Wang <jiachengwang@umass.edu>
2022-06-17 15:42:06 -0400

    test

--- project/loadcif.py
+++ project/loadcif.py
@@ -1,10 +1,18 @@
 from rdkit import Chem
 from openbabel import pybel
+from pymatgen.core.structure import Structure, Molecule
+from pymatgen.analysis.graphs import StructureGraph
+from pymatgen.analysis.local_env import JmolNN
 
 mol = next(pybel.readfile("cif", "./data/raw/Li7La3Zr2O12.cif"))
 pybelmol = pybel.Molecule(mol)
-pybelmol.write("sdf", "outputfile.sdf")
+pybelmol.write("sdf", "outputfile.sdf", overwrite=True)
 rdmol = Chem.SDMolSupplier('outputfile.sdf')
+#
+# for atom in rdmol.GetAtoms():
+#     print(atom.GetAtomicNum())  # Atom number
 
-for atom in rdmol.GetAtoms():
-    print(atom.GetAtomicNum())  # Atom number
+
+structure = Structure.from_file("data\\raw\\Li7La3Zr2O12.cif")
+sg = StructureGraph.with_local_env_strategy(structure, JmolNN())
+g = sg.graph
