fd7570b182b161cb413484bd6211e38a0f95a496
Jiacheng Wang <jiachengwang@umass.edu>
2022-06-27 12:38:26 -0400

    test

--- edgetest/loadstructure.py
+++ edgetest/loadstructure.py
@@ -12,6 +12,10 @@ import torch.nn.functional as tf
 import dgl.function as df
 import matplotlib.pyplot as plt
 
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 # Load the lattice coordinates file
 data = pd.read_csv('llto.csv')
 in_coord = data[['x', 'y', 'z']].values
@@ -68,7 +72,67 @@ for i in range(0, len(elem_dict[mov_elem])):
 
 kmc_post.dump_output(llto, 1, "./dump.llto", mode='w')
 
+
+# 1 Construct a two-layer GNN model.
+class SAGE(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats):
+        super().__init__()
+        self.conv1 = dglnn.SAGEConv(in_feats=in_feats, out_feats=hid_feats, aggregator_type='mean')
+        self.conv2 = dglnn.SAGEConv(in_feats=hid_feats, out_feats=out_feats, aggregator_type='mean')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = tf.relu(h)
+        h = self.conv2(graph, h)
+        return h
+
+
+# 2 Generate data randomly.
 edge_pred_graph = dgl.graph((np.concatenate([src, dst]), np.concatenate([dst, src])), num_nodes=54)
 gx = dgl.to_networkx(edge_pred_graph)
 nx.draw_networkx(gx)
 plt.show()
+
+edge_pred_graph.ndata['feature'] = torch.randn(54, 10)
+edge_pred_graph.edata['feature'] = torch.randn(1480, 10)
+edge_pred_graph.edata['label'] = torch.randn(1480)
+edge_pred_graph.edata['train_mask'] = torch.zeros(1480, dtype=torch.bool).bernoulli(0.6)
+
+
+# 3 Define predictor to compute feature of edge. Here gives two predictors `DotProductPredictor`.
+class DotProductPredictor(nn.Module):
+    # Compute the feature of edge by do dot production using the source node and dst
+    def forward(self, graph, h):
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(df.u_dot_v('h', 'h', 'score'))
+            return graph.edata['score']
+
+
+# 4 Define model.
+class Model(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+        self.sage = SAGE(in_features, hidden_features, out_features)
+        self.pred = DotProductPredictor()
+
+    def forward(self, g, x):
+        h = self.sage(g, x)
+        return self.pred(g, h)
+
+
+node_features = edge_pred_graph.ndata['feature']
+edge_label = edge_pred_graph.edata['label']
+train_mask = edge_pred_graph.edata['train_mask']
+
+# Train model.
+model = Model(10, 20, 5)
+opt = torch.optim.Adam(model.parameters())
+for epoch in range(100):
+    pred = model(edge_pred_graph, node_features)
+    loss = ((pred[train_mask] - edge_label[train_mask]) ** 2).mean()
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    print(loss.item())
