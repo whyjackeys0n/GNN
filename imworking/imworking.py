9271081c300f45197735a1cc91f34798a288ccde
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-07-01 12:18:08 -0400

    test

--- imworking/imworking.py
+++ imworking/imworking.py
@@ -1,16 +1,16 @@
 import win32com.client
 import time
 
-for i in range(1, 10):
+for i in range(1, 1000000):
     xlApp = win32com.client.Dispatch("Excel.Application")
     xlApp.Visible = 1
 
-    sleeptime = 1
+    sleep_time = 30
 
     workBook = xlApp.Workbooks.Open(r"D:\MyTest.xlsx")
-    time.sleep(sleeptime)
+    time.sleep(sleep_time)
     workBook.ActiveSheet.Cells(1, 1).Value = "hello"
-    time.sleep(sleeptime)
+    time.sleep(sleep_time)
     workBook.Close(SaveChanges=0)
-    time.sleep(sleeptime)
+    time.sleep(sleep_time)
     xlApp.Quit()
