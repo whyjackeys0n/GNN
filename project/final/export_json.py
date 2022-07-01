899a9d78cab30bb5dc8f00a60cc9406754895821
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-07-01 17:33:20 -0400

    test

--- project/final/export_json.py
+++ project/final/export_json.py
@@ -5,12 +5,14 @@ import json
 contcar_path = os.getcwd() + "/CONTCAR"
 contcar_list = os.listdir(contcar_path)
 
+json_path = os.getcwd() + "/data/raw"
+
 for i in range(len(contcar_list)):
     # Change the file name to CONTCAR
     os.rename(contcar_path + "/" + contcar_list[i], contcar_path + "/CONTCAR")
 
     structure_from_contcar = Structure.from_file(contcar_path + "/CONTCAR")
-    structure_from_contcar.to(filename=contcar_path + "/" + contcar_list[i] + ".json")
+    structure_from_contcar.to(filename=json_path + "/" + contcar_list[i] + ".json")
 
     # Change CONTCAR back to the file name
     os.rename(contcar_path + "/CONTCAR", contcar_path + "/" + contcar_list[i])
